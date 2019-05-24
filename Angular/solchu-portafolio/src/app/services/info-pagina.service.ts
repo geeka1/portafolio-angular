85c7b6c71b0c56c542f6bd2df69b853c3341d95e
geeka1 <mariasol.causarano@gmail.com>
2019-05-24 12:40:42 -0300

    Vamos a distribuir el codigo

--- Angular/solchu-portafolio/src/app/services/info-pagina.service.ts
+++ Angular/solchu-portafolio/src/app/services/info-pagina.service.ts
@@ -12,21 +12,44 @@ export class InfoPaginaService {
   infoObjeto: InfoPagina  = {};
 
   cargada = false;
+  equipo: any[]=[];
+
 
   constructor(private http:HttpClient) {
     console.log('Servicio Inyectado...')
-    
+    this.cargarInfoLocal();
+    this.cargarEquipoDesdeFirebase();
+   }
+
+   private cargarInfoLocal(){
     //Ahora leo el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-      .subscribe((resp:InfoPagina) =>{
-        this.cargada = true;
-        this.infoObjeto = resp;
-        console.log(resp.nombre_corto);
-        console.log('Ahora viene todo el objeto...');
-        console.log(this.infoObjeto);
-        //console.log(resp);
-        //console.log(resp['email']);
-        //console.log(resp['titulo']);
-      });
-   }
+        .subscribe((resp:InfoPagina) =>{
+          this.cargada = true;
+          this.infoObjeto = resp;
+          console.log(resp.nombre_corto);
+          console.log('Ahora viene todo el objeto...');
+          console.log(this.infoObjeto);
+          //console.log(resp);
+          //console.log(resp['email']);
+          //console.log(resp['titulo']);
+        });
+      }
+
+    private cargarEquipoDesdeFirebase(){
+
+        //Ahora leo el archivo JSON
+    this.http.get('https://console.firebase.google.com/u/0/project/solchu-firebase/database/firestore/data~2Fequipo.json')
+        .subscribe((resp:any[]) =>{
+          this.equipo = resp;
+          //console.log(resp);
+          console.log('Ahora viene todo el objeto...');
+          console.log(this.equipo);
+          //console.log(resp);
+          //console.log(resp['email']);
+          //console.log(resp['titulo']);
+        });
+  }
+
+
 }
